da901399e79f2223189488e7ee7294c398ba6a98
rachel-s123 <rachel.stubbs@brilliantnoise.com>
2025-06-25 13:38:25 +0100

    Add tests for new components

---
+++ src/__fixtures__/quotes.js
@@ -0,0 +1,22 @@
+export const sampleQuotes = [
+  {
+    id: 1,
+    text: 'Great bike for adventures.',
+    theme: 'Performance',
+    sentiment: 'Positive',
+    platform: 'forum',
+    week: '1',
+    purchaseIntent: 'Considering',
+    competitorMentioned: 'NONE'
+  },
+  {
+    id: 2,
+    text: 'Too expensive compared to competition.',
+    theme: 'Price',
+    sentiment: 'Negative',
+    platform: 'social',
+    week: '2',
+    purchaseIntent: 'Not Interested',
+    competitorMentioned: 'BrandX'
+  }
+];
